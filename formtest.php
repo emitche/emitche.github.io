8e2beaf3dec47de54b3ea262b79439ee59635601
Elizabeth Mitchell <EnjoyLife@1122800.no-reply.drupal.org>
2013-06-08 10:23:19 -0400

    Testing JavaScript form

---
+++ formtest.php
@@ -0,0 +1,11 @@
+﻿<?php
+
+if return validateForm = "true" 
+	{echo('This means the form is validated');
+	}
+	
+else { 
+	echo ('This means the form was not validated');
+	}
+
+?>
---
+++ javascript/form.js
@@ -0,0 +1,92 @@
+﻿// form.js
+
+// function validateForm {
+//	if };
+	
+// To use ECMA-262 5.1, uncomment the below line:
+"use strict"; 	
+
+var contactName = getElementById("contactName");
+var contactEmail = getElementById("contactEmail");
+var message = getElementbyId("message");
+
+function onclick(event)
+		send() {
+	if 	contactName != '' &
+		contactEmail !='' &
+		message !='' &
+		message !=''
+		
+			{alert("You are on the right track.")}
+	
+	else {
+		alert("All fields are required to send your message.")
+		}
+	}	
+
+/*
+function onclick(sendButton) {
+	if 	document.forms["contactForm"]["contactName"] != '' &
+		document.forms["contactForm"]["contactEmail"] !='' &
+		doucument.forms["contactForm"]["subject"] !='' &
+		document.forms["contactForm"]["message"] !=''
+		
+			{alert("You are on the right track."}
+	
+	else {
+		alert("All fields are required to send your message.")
+		}
+	}	
+*/		
+	
+	
+	
+/*
+
+	<script type="text/javascript">
+		"use strict"; 	
+
+		var contactName = getElementById("contactName").value;
+		var contactEmail = getElementById("contactEmail").value;
+		var message = getElementbyId("message").value;
+
+		function validateForm() {
+			if 	contactName='' || contactName=null &
+				contactEmail !='' &
+				message !='' &
+				message !=''
+				
+					{alert("You are on the right track.")}
+			
+			else {
+				alert("All fields are required to send your message.")
+				}
+			}	
+	</script>
+	
+*/
+
+
+/*
+
+<script type="text/javascript">
+		"use strict"; 	
+
+		var contactName = getElementById("contactName").value;
+		var contactEmail = getElementById("contactEmail").value;
+		var message = getElementbyId("message").value;
+
+		function validateForm() {
+			if 	(contactName='' || contactName=null)
+					{
+					alert("All fields are required to send your message.");
+					return false;
+					}
+			
+			else {
+				alert("All fields are required to send your message.")
+				}
+			}	
+	</script>
+	
+*/
